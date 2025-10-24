9a6fa9e0caf367d5d8d42f1d7f8ea91a78448498
shaquib0001 <shaquib.khan@thinkcloudly.com>
2025-10-25 00:52:42 +0530

    First commit

---
+++ Basic_01/Test_01.js
@@ -0,0 +1,27 @@
+const studentId = 101;
+var studentName = "Shaquib";
+ let studentAge = 24;
+
+
+studentName = "Alex";
+studentAge = 25;
+// studentId=201;   // Not allowed
+// console.log(studentId);
+// console.log(studentName);
+// console.log(studentAge);
+
+
+/*Output:
+101
+Alex
+25
+*/
+
+/*   
+var = Prefer Not to use var
+because of issue in block scope and function scope.
+
+*/
+
+console.table({studentId, studentName, studentAge});
+
