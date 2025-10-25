243cb0b7b4ff0dee43c9a6e06b6b4e9292442100
shaquib0001 <shaquib.khan@thinkcloudly.com>
2025-10-25 14:36:01 +0530

    fourth commit

---
+++ Basic_01/02_DataType.js
@@ -0,0 +1,3 @@
+"use-strict";  // treat All JS code as newer version
+
+alert (3+3);  // we are using nodejs not browser so alert will not work
---
+++ Basic_01/03_Conversion.js
@@ -0,0 +1,29 @@
+let studentCountry = "USA";
+
+// console.log(typeof studentCountry); 
+// console.log(typeof(studentCountry))
+
+
+let valueInNumber = Number (studentCountry);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+let isLogin="true";
+
+let booleanIsLogin=Boolean(isLogin);
+// console.log(booleanIsLogin)
+// console.log(typeof booleanIsLogin)
+
+
+let testNumber=12;
+
+let stringNumber = String(testNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+let age="34years"
+let ageInNumber=Number(age)
+console.log(ageInNumber)
+console.log(typeof ageInNumber)
